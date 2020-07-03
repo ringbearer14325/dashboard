95574a49c22e362abdeeee681bfb21ee704d5643
Daniel <danielramos1981@gmail.com>
2020-07-03 17:12:45 -0400

    Commit file.

---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Chat from "./components/Chat.js"
+import Kanban from "./components/Kanban.js"
+
+
+
+
+class Dashboard extends React.Component() {
+    
+}
+
+
+
+export default Dashboard;
---
+++ src/environments.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: 'yzRv0M18nfehYtjg4toFBzWkRL1WL2Vs6CJVDISv',
+    JAVASCRIPT_KEY: 'galWzDPyq4Pz8JY8KJ6FfO9IO9abbVOBpOfagcF1',
+    SERVER_URL: 'https://parseapi.back4app.com/'
+}
